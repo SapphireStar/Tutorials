d9f3967f3ad3ebc43e140b3094505958922f7e22
SapphireStar <1130107@wku.edu.cn>
2022-02-24 09:29:42 +0800

    Initial commit

---
+++ tutorial1.py
@@ -0,0 +1,9 @@
+name = input("please input your name ")
+age = int(input("please input your age "))
+address = input("please input your address ")
+favor = input("please input your favorite sport ")
+
+print("Hello, my name is "+name)
+print("I am ",age," years old")
+print("I live in "+address)
+print("I like to play "+favor)
